c78a8323c25ff9e5795301d4f68a92ee297106dc
SM-EDU <santhil.murugesan@pcc.edu>
2025-04-07 19:06:04 -0700

    Enhance HelloWorld.java: add welcome, greet, and fun fact methods

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,24 @@
 public class HelloWorld {
+
     public static void main(String[] args) {
-        System.out.println("Hello, CIS 233J!");
-        System.out.println("Good Bye!");
+        printWelcome();
+        String studentName = "Murugesan";
+        greetStudent(studentName);
+        showJavaFunFact();
+    }
+
+    // Method to print a welcome message
+    public static void printWelcome() {
+        System.out.println("Welcome to CIS 233J - Java Programming!");
+    }
+
+    // Method to greet the student by name
+    public static void greetStudent(String name) {
+        System.out.println("Hello, " + name + "! Great to see you learning Java.");
+    }
+
+    // Method to show a fun fact about Java
+    public static void showJavaFunFact() {
+        System.out.println("Did you know? Java was originally called Oak!");
     }
 }
